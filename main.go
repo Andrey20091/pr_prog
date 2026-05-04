1254b2b266e8c1b0c4e6debccd5b8ac8c9d6adbc
Andrew Troitskiy <andreyvolpek22@gmail.com>
2026-05-04 10:29:08 +0300

    We can min numbers now

--- main.go
+++ main.go
@@ -6,10 +6,15 @@ func sum(a, b int) int {
 	return a + b
 }
 
+func min(a, b int) int {
+	return a - b
+}
+
 func main() {
 	fmt.Println("Hello")
 	fmt.Println("It is calculate app")
 	a := 5
 	b := 8
 	fmt.Printf("Let's sum %d and %d. We have now %d\n", a, b, sum(a, b))
+	fmt.Printf("Lets min %d and %d. We have now %d\n", a, b, min(a, b))
 }
