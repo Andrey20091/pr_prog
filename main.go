d01489fa9c903a22556fe3ece89d4a8faab18c27
Andrew Troitskiy <andreyvolpek22@gmail.com>
2026-05-04 10:56:41 +0300

    Мы можем умножать

--- main.go
+++ main.go
@@ -2,7 +2,15 @@ package main
 
 import "fmt"
 
+//Умножение
+func mult(a, b int) int {
+	return a * b
+}
+
 func main() {
-	fmt.Println("Hello")
-	fmt.Println("It's time to develop something")
+	fmt.Println("привет")
+	fmt.Println("Это калькулятор")
+	a := 5
+	b := 8
+	fmt.Printf("Мы млжем умножать. Имеем %d, %d. После умножения получим %d\n", a, b, mult(a, b))
 }
