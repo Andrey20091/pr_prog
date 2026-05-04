9850d4e6d89147202aa08848de3bd6d0c0a14b1d
Andrew Troitskiy <andreyvolpek22@gmail.com>
2026-05-04 10:26:27 +0300

    We add summing in our app, fix go.mod bag

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import "fmt"
 
+func sum(a, b int) int {
+	return a + b
+}
+
 func main() {
 	fmt.Println("Hello")
-	fmt.Println("It's time to develop something")
+	fmt.Println("It is calculate app")
+	a := 5
+	b := 8
+	fmt.Printf("Let's sum %d and %d. We have now %d\n", a, b, sum(a, b))
 }
